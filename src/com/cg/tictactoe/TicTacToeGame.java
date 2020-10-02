e92221838ff2ee042ec2a077e244a2e26082d6aa
Abhinav9p16 <thakurabhinav56@gmail.com>
2020-10-03 02:26:57 +0530

    Create Board

--- src/com/cg/tictactoe/TicTacToeGame.java
+++ src/com/cg/tictactoe/TicTacToeGame.java
@@ -1,7 +1,22 @@
 package com.cg.tictactoe;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
+	static char[] board = new char[10];
+	static Scanner sc = new Scanner(System.in);
+
+	private static char[] createBoard() {
+		for (int i = 1; i < 10; i++) {
+			board[i] = ' ';
+		}
+		return board;
+	}
+	
+	
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to TicTacToe Game");
+		board = createBoard();
 	}
 }
